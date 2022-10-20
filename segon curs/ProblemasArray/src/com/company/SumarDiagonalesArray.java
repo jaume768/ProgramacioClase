0380d716f4f9ed69a06e1cde7333e073baf6a0be
jaume768 <jaumefernandezsunyer10@gmail.com>
2022-10-20 12:58:22 +0200

    push

---
+++ segon curs/ProblemasArray/src/com/company/SumarDiagonalesArray.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.util.Scanner;
+
+public class SumarDiagonalesArray {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[][] Array = new int[2][2];
+        Array = RellenarArray(Array);
+
+        System.out.println(SumarFiles(1,Array));
+
+    }
+
+    public static int[][] RellenarArray(int[][] Array){
+
+        for (int i = 0; i < Array.length; i++) {
+            for (int j = 0; j < Array.length; j++) {
+
+                System.out.println("Dime el valor de la posición " + i + " " + j + ": ");
+                Array[i][j] = sc.nextInt();
+
+            }
+
+        }
+
+        return Array;
+
+    }
+
+    public static int SumarFiles(int Fila,int[][] Array){
+
+        int Suma = 0;
+
+        for (int i = 0; i < Array.length; i++) {
+
+            Suma += Array[Fila][i];
+
+        }
+
+        return Suma;
+
+    }
+
+}
